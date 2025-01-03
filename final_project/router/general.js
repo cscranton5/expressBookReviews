00fbc03d9a8508214a939c112751302e962b2fa3
Cscranton5 <CarterScranton5@gmail.com>
2025-01-03 13:14:59 -0500

    Get book by tile endpoint implimented

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -38,8 +38,12 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let booksArray = Object.values(books);
+
+    let title = req.params.title;
+    let booksByTitle = booksArray.filter(book => book.title === title);
+  
+    return res.status(200).send(JSON.stringify(booksByTitle, null, 4));
 });
 
 //  Get book review
