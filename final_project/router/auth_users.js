f73fc508beb430eaf09eebb05bbb1873ba7e0f14
Cscranton5 <CarterScranton5@gmail.com>
2025-01-03 16:34:20 -0500

    Midway on implimenting review endpoint

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -10,19 +10,51 @@ const isValid = (username)=>{ //returns boolean
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    return users.some((user) => { return (user.username === username && user.password === password) })
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+
+    if (username && password) {
+        if (authenticatedUser(username, password)) {
+            let accessToken = jwt.sign({
+                data: username
+            }, 'access', { expiresIn: 60 * 60 });
+
+            // Store access token in session
+            req.session.authorization = {
+                accessToken
+            }
+            return res.status(200).send("User successfully logged in");
+        }else{
+            return res.status(422).json({message: "Login Failed"});
+        }
+    }else
+    {
+        return res.status(400).json({message: "username and-or password not given"});
+    }
+
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+    
+    const book = books[req.params.isbn];
+    const username = jwt.decode(req.session.accessToken).data;
+
+    if(book){
+        book["reviews"][username] = req.query.review;
+        res.status(200).json({message: "review added"})
+    }else{
+        res.status(402).json({message:"Invalid ISBN"})
+    }
+
 });
 
 module.exports.authenticated = regd_users;
